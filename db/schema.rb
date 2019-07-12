6c881a6f21fdac660d987980d50317c7169d96cb
Andy Parinas <andy.parinas@gmail.com>
2019-07-12 18:25:03 +1000

    Desgin Products index and forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_023007) do
+ActiveRecord::Schema.define(version: 2019_07_12_050538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2019_07_09_023007) do
     t.decimal "tax", default: "0.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.text "description"
   end
 
   add_foreign_key "order_items", "orders"
